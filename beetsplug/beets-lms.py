d298c39a8fe3e116cc92841c7a6fdcb88cee7d3b
InvisibleFunction <3682184+InvisibleFunction@users.noreply.github.com>
2024-11-18 09:02:55 -0500

    Cleanup

--- beetsplug/beets-lms.py
+++ beetsplug/beets-lms.py
@@ -4,6 +4,7 @@ Beets Plugin to facilitate interactions with Lyrion Music Server
 
 from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand
+from beets.util import ConfigError
 from beets import config
 import requests
 import json
@@ -11,23 +12,23 @@ from pathlib import Path
 
 from icecream import ic
 
+
 class BeetsLMSPlugin(BeetsPlugin):
     # Default headers for HTTP requests
     headers = {"Content-Type": "application/json"}
 
     def __init__(self):
         super(BeetsLMSPlugin, self).__init__()
-        #self.register_listener("import", self._rescan_library)
-        self.register_listener("album_imported", self._rescan_album)
 
-        # Configuration for LMS server
-        config['beets_lms'].add(
-                {
-                    "host": "localhost",
-                    "port": "9000",
-                    "secure": False,
-                    "library_root": "/music/",
-                }
+        # Default plugin configuration
+        config["beets_lms"].add(
+            {
+                "host": "localhost",
+                "port": "9000",
+                "secure": False,
+                "library_root": "/music/",
+                "listener_method": "full",
+            }
         )
         self.host = config["beets_lms"]["host"].as_str()
         self.port = config["beets_lms"]["port"].as_str()
@@ -35,9 +36,19 @@ class BeetsLMSPlugin(BeetsPlugin):
             self.proto = "https"
         else:
             self.proto = "http"
-
         self.server_url = f"{self.proto}://{self.host}:{self.port}/jsonrpc.js"
         self.library_root = config["beets_lms"]["library_root"].as_str()
+
+        self.listener_method = (
+            config["beets_lms"]["listener_method"].as_str().to_lower()
+        )
+        ic(self.listener_method)
+        if self.listener_method == "full":
+            self.register_listener("import", self._rescan_library)
+        if self.listener_method == "path":
+            self.register_listener("album_imported", self._rescan_album)
+        else:
+            raise ConfigError("listener_method must be full or path")
 
     def commands(self):
         """
@@ -47,21 +58,28 @@ class BeetsLMSPlugin(BeetsPlugin):
         rescan_library_cmd = Subcommand(
             "lmsrescan", help="Issue a rescan command to the LMS"
         )
+
         def rescan_library(lib, opts, args):
             self._rescan_library()
+
         rescan_library_cmd.func = rescan_library
 
         # Initiate a rescan with a path
         rescan_path_cmd = Subcommand(
-            "lmspathscan", help="Issue a rescan command with a path to the LMS")
+            "lmspathscan", help="Issue a rescan command with a path to the LMS"
+        )
+
         def rescan_path(lib, opts, args):
             self._rescan_path(lib, opts, args)
+
         rescan_path_cmd.func = rescan_path
 
         # Print if LMS is currently scanning the library.
         scan_status_cmd = Subcommand("lmsstatus", help="Is LMS currently scanning?")
+
         def scan_status(lib, opts, args):
             self._scan_status()
+
         scan_status_cmd.func = scan_status
 
         return [rescan_library_cmd, rescan_path_cmd, scan_status_cmd]
@@ -74,16 +92,9 @@ class BeetsLMSPlugin(BeetsPlugin):
 
     def _rescan_album(self, lib, album):
         full_item_path = Path(album.item_dir().decode("utf-8"))
-        ic(full_item_path)
         root_directory = Path(config["directory"].as_str())
-        ic(root_directory)
         rel_path = full_item_path.relative_to(root_directory)
-        ic(rel_path)
         self.trigger_rescan(path=rel_path)
-
-    def _rescan_path(self, lib, opts, args):
-        print("not implemented")
-        pass
 
     def _scan_status(self):
         if self.is_currently_scanning():
