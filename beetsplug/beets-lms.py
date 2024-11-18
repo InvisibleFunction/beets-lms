1bf0e16c286d63a0d66f4a4c8792b674f8f782eb
InvisibleFunction <3682184+InvisibleFunction@users.noreply.github.com>
2024-11-17 22:19:11 -0500

    Add path scan

--- beetsplug/beets-lms.py
+++ beetsplug/beets-lms.py
@@ -7,6 +7,9 @@ from beets.ui import Subcommand
 from beets import config
 import requests
 import json
+from pathlib import Path
+
+from icecream import ic
 
 class BeetsLMSPlugin(BeetsPlugin):
     # Default headers for HTTP requests
@@ -14,11 +17,27 @@ class BeetsLMSPlugin(BeetsPlugin):
 
     def __init__(self):
         super(BeetsLMSPlugin, self).__init__()
-        self.register_listener("import", self._rescan_library)
+        #self.register_listener("import", self._rescan_library)
+        self.register_listener("album_imported", self._rescan_album)
 
         # Configuration for LMS server
+        config['beets_lms'].add(
+                {
+                    "host": "localhost",
+                    "port": "9000",
+                    "secure": False,
+                    "library_root": "/music/",
+                }
+        )
         self.host = config["beets_lms"]["host"].as_str()
-        self.server_url = f"http://{self.host}:9000/jsonrpc.js"
+        self.port = config["beets_lms"]["port"].as_str()
+        if config["beets_lms"]["secure"].get():
+            self.proto = "https"
+        else:
+            self.proto = "http"
+
+        self.server_url = f"{self.proto}://{self.host}:{self.port}/jsonrpc.js"
+        self.library_root = config["beets_lms"]["library_root"].as_str()
 
     def commands(self):
         """
@@ -32,19 +51,39 @@ class BeetsLMSPlugin(BeetsPlugin):
             self._rescan_library()
         rescan_library_cmd.func = rescan_library
 
+        # Initiate a rescan with a path
+        rescan_path_cmd = Subcommand(
+            "lmspathscan", help="Issue a rescan command with a path to the LMS")
+        def rescan_path(lib, opts, args):
+            self._rescan_path(lib, opts, args)
+        rescan_path_cmd.func = rescan_path
+
         # Print if LMS is currently scanning the library.
         scan_status_cmd = Subcommand("lmsstatus", help="Is LMS currently scanning?")
         def scan_status(lib, opts, args):
             self._scan_status()
         scan_status_cmd.func = scan_status
 
-        return [rescan_library_cmd, scan_status_cmd]
+        return [rescan_library_cmd, rescan_path_cmd, scan_status_cmd]
 
     def _rescan_library(self):
         if self.is_currently_scanning():
             self._log.info("LMS library scan already in progress. Skipping rescan.")
         else:
             self.trigger_rescan()
+
+    def _rescan_album(self, lib, album):
+        full_item_path = Path(album.item_dir().decode("utf-8"))
+        ic(full_item_path)
+        root_directory = Path(config["directory"].as_str())
+        ic(root_directory)
+        rel_path = full_item_path.relative_to(root_directory)
+        ic(rel_path)
+        self.trigger_rescan(path=rel_path)
+
+    def _rescan_path(self, lib, opts, args):
+        print("not implemented")
+        pass
 
     def _scan_status(self):
         if self.is_currently_scanning():
@@ -74,21 +113,27 @@ class BeetsLMSPlugin(BeetsPlugin):
             self._log.error("Failed to check scan status: {}", e)
             return False
 
-    def trigger_rescan(self):
+    def trigger_rescan(self, path=None):
         """
         Initiates a rescan of the LMS library
         """
         # https://lyrion.org/reference/cli/database/#rescan
+        if not path:
+            commands = ["rescan"]
+        else:
+            commands = ["rescan", "full", f"file://{self.library_root}{path}"]
+        ic(commands)
         payload = {
             "id": 1,
             "method": "slim.request",
-            "params": [0, ["rescan"]],
+            "params": [0, commands],
         }
 
         try:
             response = requests.post(
                 self.server_url, headers=self.headers, data=json.dumps(payload)
             )
+            ic(response)
             response.raise_for_status()
             self._log.info("LMS library rescan triggered.")
         except requests.exceptions.RequestException as e:
