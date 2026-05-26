b2dcab70255151c7dbe424c6d4ab127bb21437ec
InvisibleFunction <3682184+InvisibleFunction@users.noreply.github.com>
2026-05-25 23:37:08 -0400

    Try to not break stuff

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='beets-lms',
-    version='0.0.2',
+    version='0.0.3',
     description='beets plugin for Lyrion Music Server',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url='https://github.com/InvisibleFunction/beets-lms',
     license='MIT',
     platforms='ALL',
-    packages=['beetsplug'],
+    packages=find_namespace_packages(include=['beetsplug']),
     install_requires=[
-        'beets>=1.6.0',
+        'beets>=2.4.0',
         'requests'
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.11",
 )
